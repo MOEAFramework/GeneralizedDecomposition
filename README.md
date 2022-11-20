e650bdaaf6009ecdd2cfcea103297a60028ed970
Dave Hadka <dhadka@github.com>
2022-11-20 07:40:08 -0600

    Turn into provider

--- README.md
+++ README.md
@@ -1,8 +1,65 @@
 # Generalized Decomposition
 
-Demonstrates how to use generalized decomposition (GD) within the [MOEA Framework](http://github.com/MOEAFramework/MOEAFramework).
-GD determines the correct Chebychev (Tchebycheff) scalarizing function weights that attain solutions near a given set of target
-points.  The weights produced by GD can be fed into optimization algorithms, such as MOEA/D, to produce more desirable results
-compared to randomly-generated weights.
+Extends the [MOEA Framework](http://github.com/MOEAFramework/MOEAFramework) with a version of MOEA/D using weights produced by
+Generalized Decomposition (GD).
 
-The example provided in this repo creates and compares MOEA/D against this new version with generalized decomposition, named GD-MOEA/D.
+> Giagkiozis, I., R. C. Purshouse, and P. J. Fleming (2013). "Generalized Decomposition."  Evolutionary Multi-Criterion
+> Optimization, 7th International Conference, pp. 428-442.
+
+Very briefly, MOEA/D typically uses randomly-generated weights that aim to produce a diverse set of solutions.  However, on
+problems with disjoint or oddly shaped Pareto fronts, these weights might not lead to uniformly-spaced solutions.  Additionally,
+modern algorithms like NSGA-III have the option to supply "target points", allowing the algorithm to focus and discover
+solutions near these targets.
+
+GD addresses these concerns by determining the correct weights (for the Chebychev / Tchebycheff scalarizing function used by
+MOEA/D) needed to produce Pareto solutions near a given set of target points.  This way, if we know approximately the location or
+shape of the Pareto front, we can direct the optimization algorithm towards that region.
+
+## Usage
+
+To use, reference the `GD-MOEA/D` algorithm and supply a `"targets"` property pointing to a file containing the target
+points (objective values only).  If no `"targets"` is provided, it defaults to targeting uniformly-spaced points generated
+by the Normal Boundary Intersection method.
+
+```java
+
+NondominatedPopulation result = new Executor()
+		.withProblem("DTLZ2_3")
+		.withAlgorithm("GD-MOEA/D")
+		.withProperty("targets", "pf/DTLZ2.3D.pf")  // Target Pareto-optimal points
+		.withMaxEvaluations(10000)
+		.run();
+```
+
+## Bundling with the MOEA Framework
+
+To use this algorithm within the MOEA Framework, first compile and package this project using Maven:
+
+```
+
+mvn package
+```
+
+Then, copy the `.jar` file, typically `target/gd-1.0.0.jar`, into the `lib/` directory of the MOEA Framework.
+You can then reference this algorithm as demonstrated in the example above.
+
+
+The resulting algorithm, called `GD-MOEA/D`, can be used within the MOEA Framework by including this library in the
+classpath.
+
+## License
+
+Copyright 2009-2022 David Hadka and other contributors.  All rights reserved.
+
+The MOEA Framework is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+The MOEA Framework is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the MOEA Framework.  If not, see <http://www.gnu.org/licenses/>.
--- src/main/java/org/moeaframework/gd/Example.java
+++ src/main/java/org/moeaframework/gd/Example.java
@@ -1,41 +1,11 @@
 package org.moeaframework.gd;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.moeaframework.Analyzer;
 import org.moeaframework.Executor;
-import org.moeaframework.algorithm.MOEAD;
-import org.moeaframework.core.Algorithm;
-import org.moeaframework.core.FrameworkException;
-import org.moeaframework.core.Initialization;
-import org.moeaframework.core.Population;
-import org.moeaframework.core.PopulationIO;
-import org.moeaframework.core.Problem;
-import org.moeaframework.core.Solution;
-import org.moeaframework.core.Variation;
-import org.moeaframework.core.operator.RandomInitialization;
-import org.moeaframework.core.spi.AlgorithmFactory;
-import org.moeaframework.core.spi.OperatorFactory;
-import org.moeaframework.core.spi.RegisteredAlgorithmProvider;
-import org.moeaframework.util.TypedProperties;
-import org.moeaframework.util.weights.NormalBoundaryDivisions;
-import org.moeaframework.util.weights.NormalBoundaryIntersectionGenerator;
-import org.moeaframework.util.weights.WeightGenerator;
 
 public class Example {
 	
 	public static void main(String[] args) {
-		AlgorithmFactory.getInstance().addProvider(new RegisteredAlgorithmProvider() {
-			
-			{
-				register(Example::newGDMOEAD, "GD-MOEAD", "GD-MOEA/D");
-			}
-			
-		});
-		
 		Executor executor = new Executor()
 				.withProblem("DTLZ2_3")
 				.withMaxEvaluations(10000);
@@ -55,78 +25,11 @@ public class Example {
 		// points, we should get extremely close to the optimum
 		analyzer.addAll("GD-MOEA/D", executor
 				.withAlgorithm("GD-MOEA/D")
-				.withProperty("operator", "de+pm") // this is the default for MOEA/D
-				.withProperty("targets", "DTLZ2.3D.pf")
+				.withProperty("targets", "pf/DTLZ2.3D.pf")
 				.runSeeds(50));
 		
 		analyzer.printAnalysis();
 
-	}
-	
-	private static Algorithm newGDMOEAD(TypedProperties properties, Problem problem) {
-		int populationSize;
-		
-		//provide weights
-		WeightGenerator weightGenerator = null;
-		
-		if (properties.contains("targets")) {
-			try {
-				Population population = PopulationIO.readObjectives(
-						new File(properties.getString("targets", null)));
-				
-				List<double[]> targets = new ArrayList<double[]>();
-				
-				for (Solution solution : population) {
-					targets.add(solution.getObjectives());
-				}
-				
-				populationSize = targets.size();
-				weightGenerator = new GeneralizedDecomposition(targets);
-			} catch (IOException e) {
-				throw new FrameworkException("failed to load targets", e);
-			}
-		} else {
-			NormalBoundaryDivisions divisions = NormalBoundaryDivisions.fromProperties(properties, problem);
-			
-			populationSize = divisions.getNumberOfReferencePoints(problem);
-			weightGenerator = new GeneralizedDecomposition(new NormalBoundaryIntersectionGenerator(
-					problem.getNumberOfObjectives(), divisions));
-		}
-		
-		//enforce population size lower bound
-		if (populationSize < problem.getNumberOfObjectives()) {
-			System.err.println("increasing MOEA/D population size");
-			populationSize = problem.getNumberOfObjectives();
-		}
-
-		Initialization initialization = new RandomInitialization(problem, populationSize);
-		Variation variation = OperatorFactory.getInstance().getVariation(null, properties, problem);
-		
-		int neighborhoodSize = 20;
-		int eta = 2;
-		
-		if (properties.contains("neighborhoodSize")) {
-			neighborhoodSize = Math.max(2, 
-					(int)(properties.getDouble("neighborhoodSize", 0.1) * populationSize));
-		}
-		
-		if (neighborhoodSize > populationSize) {
-			neighborhoodSize = populationSize;
-		}
-		
-		if (properties.contains("eta")) {
-			eta = Math.max(2, (int)(properties.getDouble("eta", 0.01) * populationSize));
-		}
-		
-		return new MOEAD(
-				problem,
-				neighborhoodSize,
-				weightGenerator,
-				initialization,
-				variation,
-				properties.getDouble("delta", 0.9),
-				eta,
-				(int)properties.getDouble("updateUtility", -1));
 	}
 
 }
